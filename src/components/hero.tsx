0d7a361267db66b2ef3ee1e46e6ce2e26cff31d3
Joshua Akande <joshuaayomikun@gmail.com>
2022-02-09 07:44:39 +0100

    font size

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,19 +2,18 @@ import { Container, styled, Typography } from "@mui/material";
 import { FC } from "react";
 import AppContainer from "./app-container";
 
-const HeroTypography = styled(Typography)({
-    fontSize: '200px',
-    textAlign:'center',
-    fontWeight:'bolder',
-    textTransform:'uppercase',
-    color: '#96abc7'
-})
 
-const Hero:FC = () => {
+const Hero: FC = () => {
     return <AppContainer>
-        <HeroTypography>
+        <Typography sx={{
+            fontSize: { md: '150px', lg: '200px'},
+            textAlign: 'center',
+            fontWeight: 'bolder',
+            textTransform: 'uppercase',
+            color: '#96abc7'
+        }}>
             our services
-        </HeroTypography>
+        </Typography>
     </AppContainer>
 }
 
